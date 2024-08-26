941b8558e06368ddca64f238d5ce013d6bc4c22a
zhangzeyu <v_zhangzeyu.js@chinatelecom.cn>
2024-08-26 19:14:56 +0800

    nacos学习

--- SpringCloudStudy/user-service/src/main/java/com/zzy/controller/UserController.java
+++ SpringCloudStudy/user-service/src/main/java/com/zzy/controller/UserController.java
@@ -2,6 +2,8 @@ package com.zzy.controller;
 
 import com.zzy.entity.User;
 import com.zzy.service.UserService;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,14 +11,20 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 
 @RestController
+@RefreshScope
 public class UserController {
 
     @Resource
     UserService service;
 
+    @Value("${test.test-content}")
+    String testContent;
+
     //这里以RESTFul风格为例
     @RequestMapping("/user/{uid}")
     public User findUserById(@PathVariable("uid") int uid){
+        System.out.println(testContent);
+        System.out.println("UserController");
         return service.getUserById(uid);
     }
 }
