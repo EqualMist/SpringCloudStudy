2c4cc80059563f5847e615f5bdab09b568a16cde
zhangzeyu <v_zhangzeyu.js@chinatelecom.cn>
2024-08-27 20:27:28 +0800

    sentinel学习

--- SpringCloudStudy/borrow-service/src/main/java/com/zzy/controller/BorrowController.java
+++ SpringCloudStudy/borrow-service/src/main/java/com/zzy/controller/BorrowController.java
@@ -1,12 +1,17 @@
 package com.zzy.controller;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+import com.alibaba.fastjson.JSONObject;
 import com.zzy.entity.BorrowDetail;
 import com.zzy.service.BorrowService;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 
 @RestController
 public class BorrowController {
@@ -20,4 +25,44 @@ public class BorrowController {
         return borrowDetail;
     }
 
+    @RequestMapping("/borrow2/{uid}")
+    BorrowDetail findUserBorrows2(@PathVariable("uid") int uid){
+        BorrowDetail borrowDetail =service.getUserBorrowDetailByUid(uid);
+        return borrowDetail;
+    }
+
+    @RequestMapping("/blocked")
+    JSONObject Blocked(){
+        JSONObject jsonObject=new JSONObject();
+        jsonObject.put("code",403);
+        jsonObject.put("success", false);
+        jsonObject.put("msg","您的请求频率过快，请稍后再试");
+        return jsonObject;
+    }
+
+    @RequestMapping("/test")
+    @SentinelResource(value = "test",
+            fallback = "except",    //fallback指定出现异常时的替代方案
+            blockHandler = "blockMethod",
+            exceptionsToIgnore = IOException.class)  //忽略那些异常，也就是说这些异常出现时不使用替代方案
+    String test(){
+        return"HelloWorld！";
+    }
+
+    //替代方法必须和原方法返回值和参数一致，最后可以添加一个Throwable作为参数接受异常
+    String except(Throwable t){
+        return t.getMessage();
+    }
+
+    String blockMethod(BlockException e) {
+        return "blocked";
+    }
+
+    @RequestMapping("/testParam")
+    @SentinelResource("testParam")   //注意这里需要添加@SentinelResource才可以，用户资源名称就使用这里定义的资源名称
+    String findUserBorrows2(@RequestParam(value = "a", required = false) String a,
+                            @RequestParam(value = "b", required = false) String b,
+                            @RequestParam(value = "c",required = false) String c) {
+        return "请求成功！a = "+a+", b = "+b+", c = "+c;
+    }
 }
--- SpringCloudStudy/borrow-service/src/main/java/com/zzy/service/impl/BorrowServiceImpl.java
+++ SpringCloudStudy/borrow-service/src/main/java/com/zzy/service/impl/BorrowServiceImpl.java
@@ -1,5 +1,7 @@
 package com.zzy.service.impl;
 
+import com.alibaba.csp.sentinel.annotation.SentinelResource;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.zzy.entity.Book;
 import com.zzy.entity.Borrow;
 import com.zzy.entity.BorrowDetail;
@@ -12,6 +14,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -26,6 +29,7 @@ public class BorrowServiceImpl implements BorrowService {
     @Resource
     BookClient bookClient;
 
+    @SentinelResource(value = "borrowServiceImpl", blockHandler = "blocked")
     @Override
     public BorrowDetail getUserBorrowDetailByUid(int uid) {
         List<Borrow> borrowList = mapper.getBorrowsByUid(uid);
@@ -39,6 +43,11 @@ public class BorrowServiceImpl implements BorrowService {
                 .map(borrow -> bookClient.findBookById(borrow.getBid()))
                 .collect(Collectors.toList());
         return new BorrowDetail(user, bookList);
+    }
+
+    // 替代方案，注意参数和返回值需要保持一致，并且参数最后还需要额外添加一个BlockException
+    public BorrowDetail blocked(int uid, BlockException e) {
+        return new BorrowDetail(null, Collections.emptyList());
     }
 
 }
